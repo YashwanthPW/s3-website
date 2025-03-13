71d12804d17bbb2d93b120c8642b67316512a6d2
Yashwanth <123480564+YashwanthPW@users.noreply.github.com>
2025-03-13 09:52:32 +0530

    Upda s3-0bject-upload.tf

--- s3-0bject-upload.tf
+++ s3-0bject-upload.tf
@@ -1,4 +1,7 @@
 
+data "aws_s3_bucket" "selected-bucket" {
+  bucket = aws_s3_bucket.bucket-1.bucket
+}
 resource "aws_s3_object" "object-upload-html" {
   bucket = data.aws_s3_bucket.selected-bucket.id  # Correct reference
   key    = "index.html"
