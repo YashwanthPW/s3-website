c0c0bfcfe1839289396ab2ecb628f7458768368d
Yashwanth <123480564+YashwanthPW@users.noreply.github.com>
2025-03-13 10:03:18 +0530

    Update s3-0bject-upload.tf

--- s3-0bject-upload.tf
+++ s3-0bject-upload.tf
@@ -1,6 +1,9 @@
 
 data "aws_s3_bucket" "selected-bucket" {
   bucket = aws_s3_bucket.static_site.bucket
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 resource "aws_s3_object" "object-upload-html" {
   bucket = data.aws_s3_bucket.selected-bucket.id  # Correct reference
