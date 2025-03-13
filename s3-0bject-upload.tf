ada6452978af6b0cc3fe64d0f7a3d8eaf843a3c5
Yashwanth <123480564+YashwanthPW@users.noreply.github.com>
2025-03-13 09:46:46 +0530

    Update s3-0bject-upload.tf

--- s3-0bject-upload.tf
+++ s3-0bject-upload.tf
@@ -1,6 +1,4 @@
-data "aws_s3_bucket" "selected-bucket" {
-  bucket = "100-my-static-website-${random_string.suffix.result}"
-}
+
 resource "aws_s3_object" "object-upload-html" {
   bucket = data.aws_s3_bucket.selected-bucket.id  # Correct reference
   key    = "index.html"
